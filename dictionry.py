020e9c6477cd37574e23aadb38050d76f42878ec
sairayasir <sairayasir1498@gmail.com>
2023-06-25 18:53:41 -0700

    dictionary updated

--- dictionry.py
+++ dictionry.py
@@ -1,4 +1,3 @@
-
 phonebook ={}
 phonebook["saira"] = 3175008890
 phonebook["yasir"] = 3175008894
@@ -11,6 +10,7 @@ phonebook = {
      "yasir" : 3175008894,
      "sheeri": 3175008896
 }
+
 print(phonebook)
 #iterating over dictionaries
 for name, number in phonebook.items():
@@ -19,3 +19,25 @@ for name, number in phonebook.items():
 #removing a value
 del phonebook["yasir"]
 print(phonebook)
+
+phonebook.pop("saira")
+print(phonebook)
+
+
+#Exercise
+phonebook = {  
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}  
+
+# your code goes here
+phonebook["Jake"] = 938273443  
+del phonebook["Jill"]  
+
+# testing code
+if "Jake" in phonebook:  
+    print("Jake is listed in the phonebook.")
+    
+if "Jill" not in phonebook:      
+    print("Jill is not listed in the phonebook.")  
